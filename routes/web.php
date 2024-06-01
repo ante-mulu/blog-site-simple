4e66d035c5eb3f72095ddc6c88f1033644556b33
anteneh <anteneh.mulu1921@gmail.com>
2024-06-01 06:07:07 -0400

    customizing bootstrap template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// // Route::get('/second', function(){
+// //     return view('second');
+// // });
+// Route::view('/second', 'second');
+Route::get('/home', function(){return view('home');});
