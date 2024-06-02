dbe3b5673ca3e832f6d2fb40460adbbe04e171a0
anteneh <anteneh.mulu1921@gmail.com>
2024-06-02 07:28:45 -0400

    Design Admin Page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = ['title', 'text', 'category_id'];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::middleware('is_admin')->group(function () {
+        Route::resource('categories',CategoryController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('posts', PostController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
